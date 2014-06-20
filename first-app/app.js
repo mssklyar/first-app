27c3f885c57387584d32d4c719994704fa501aaf
Sklyar Max <ravonta@yandex.ru>
2014-06-20 21:27:47 +0700

    New Files

--- first-app/app.js
+++ first-app/app.js
@@ -1,36 +1,71 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
 
-/**
- * Module dependencies.
+var app = express();
+app.set('port', 3000);
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+// Middleware
+app.use(function(req, res, next) {
+    if (req.url == '/') {
+        res.end("Hello");
+    } else {
+        next();
+    }
+});
+
+app.use(function(req, res, next) {
+    if (req.url == '/forbidden') {
+        next(new Error("wops, denied"));
+    } else {
+        next();
+    }
+});
+
+app.use(function(req, res, next) {
+    if (req.url == '/test') {
+        res.end("Test");
+    } else {
+        next();
+    }
+});
+
+app.use(function(req, res) {
+    res.send(404, "Page Not Found Sorry");
+});
+
+app.use(function(err, req, res, next) {
+    // NODE_ENV = 'production'
+    if (app.get('env') == 'development') {
+        var errorHandler = express.errorHandler();
+        errorHandler(err, req, res, next);
+    } else {
+        res.send(500);
+    }
+});
+/*
+
+ var routes = require('./routes');
+ var user = require('./routes/user');
+
+ // all environments
+ app.set('port', process.env.PORT || 3000);
+ app.set('views', __dirname + '/views');
+ app.set('view engine', 'ejs');
+ app.use(express.favicon());
+ app.use(express.logger('dev'));
+ app.use(express.bodyParser());
+ app.use(express.methodOverride());
+ app.use(express.cookieParser('your secret here'));
+ app.use(express.session());
+ app.use(app.router);
+ app.use(express.static(path.join(__dirname, 'public')));
+
+ app.get('/', routes.index);
+ app.get('/users', user.list);
+
  */
-
-var express = require('express')
-  , routes = require('./routes');
-
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
